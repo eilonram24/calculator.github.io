fcc29064d1206173f9b4c857d97430528a5759d8
Eilonram <92418683+Eilonram@users.noreply.github.com>
2022-03-25 17:25:09 +0800

    calculate script

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-let display = document.getElementById('display');
-let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
-                display.innerText += e.target.innerText;
-        }
-    });
-});
+function totalIt() {
+    var input = document.getElementsByName("checkbox");
+    var total = 0;
+    var value1 = document.getElementById("rangenumber").value;
+    for (var i = 0; i < input.length; i++) {
+      if (input[i].checked) {
+        total += parseFloat(input[i].value);
+      }
+    }
+    element = document.querySelector('.inputshow');
+    element.style.visibility = 'visible';
+    document.getElementsByName("total")[0].value = "Php" + total.toFixed(2) * value1;
+  }
